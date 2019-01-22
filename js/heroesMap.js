3c31119be54aab078fbd3aa723d416c212ce2523
Josh Shepherd <josh_lmao@live.co.uk>
2019-01-22 16:13:42 +0000

    Lowered example data, removed images. Uses json map

---
+++ js/heroesMap.js
@@ -0,0 +1,196 @@
+const heroesMap  = {
+    "heroesMap": [
+      {
+        "HeroName": "Farvhan the Dreamer",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set00/4000_ingame.cb1d509e48befec604e5ca48a6fe0752ff2ff1e5.png"
+      },
+      {
+        "HeroName": "Keefe the Bold",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set00/4003_ingame.754e1c3ccfaa3b0b0921ff0fba171aef6d343ca8.png"
+      },
+      {
+        "HeroName": "Debbi the Cunning",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set00/4005_ingame.fa80d8637ebe79e37f2e6b332de59de4d7bc568a.png"
+      },
+      {
+        "HeroName": "J'Muy the Wise",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set00/4008_ingame.094a2c09317b5d7b0dbce840c3384811912e0e06.png"
+      },
+      {
+        "HeroName": "Venomancer",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10001_ingame.1c347d3e7359e39d7ca61f6add8b06604b9ecffd.png"
+      },
+      {
+        "HeroName": "Meepo",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10004_ingame.abad49f30c45897120d06d3bef0db20798800c49.png"
+      },
+      {
+        "HeroName": "Luna",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10006_ingame.89d98455aa3b6db2aac61d258774b198540b7dbd.png"
+      },
+      {
+        "HeroName": "Winter Wyvern",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10010_ingame.633228c1be0c51fb3416a901351e42397ccc9912.png"
+      },
+      {
+        "HeroName": "Lycan",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10014_ingame.1a79bc32dfc3ba6a9b73887d6db75704ef9d14df.png"
+      },
+      {
+        "HeroName": "Abaddon",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10016_ingame.e554b0820026fab65e0d93a6c236f3971ff9aa0d.png"
+      },
+      {
+        "HeroName": "Chen",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10017_ingame.6ebdad88afa95fd63b1833564028e432b0ac2d30.png"
+      },
+      {
+        "HeroName": "Bloodseeker",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10018_ingame.e30c301154cb882531366a397486b99bdadf4004.png"
+      },
+      {
+        "HeroName": "Axe",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10020_ingame.4b1773e0546ae29327b3d1be27455ca6b38bc4bc.png"
+      },
+      {
+        "HeroName": "Centaur Warrunner",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10021_ingame.ef3cf78a69b914097f4cf8ad54b89cc03a96f4e9.png"
+      },
+      {
+        "HeroName": "Timbersaw",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10022_ingame.298367b2c0a9d5a83a290c4b26c4db8118bc1f0a.png"
+      },
+      {
+        "HeroName": "Bounty Hunter",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10023_ingame.98f5e792764681cf0d2662d6e38a8ff430404cb7.png"
+      },
+      {
+        "HeroName": "Tidehunter",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10024_ingame.79325d3e4ff8841ba2833786d5d7c3bc9f3a5d8a.png"
+      },
+      {
+        "HeroName": "Tinker",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10025_ingame.21005804eb139e25697a143575fe5b0cac36c579.png"
+      },
+      {
+        "HeroName": "Rix",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10026_ingame.0b596f4469b0ad3050552849284804a8d1a986be.png"
+      },
+      {
+        "HeroName": "Viper",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10028_ingame.f78ba0c0871819c7763bd3e7321554d176b7903a.png"
+      },
+      {
+        "HeroName": "Beastmaster",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10029_ingame.0a80b175673b135a779857bd0fa9b347b5082144.png"
+      },
+      {
+        "HeroName": "Bristleback",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10030_ingame.3637b79cc931a18328e3d324ee1a61dfc4e2c35b.png"
+      },
+      {
+        "HeroName": "Kanna",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10031_ingame.f73eb3453e6882b39142225ac45c2ea7a1686044.png"
+      },
+      {
+        "HeroName": "Drow Ranger",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10032_ingame.7fbb9f47159e639d43413a9f8dc37dc87f895bbf.png"
+      },
+      {
+        "HeroName": "Earthshaker",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10033_ingame.12e1d59de9862d5f8ede3c0014fad6b7bf2ad3ff.png"
+      },
+      {
+        "HeroName": "Enchantress",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10036_ingame.f65dfe92226c5bb9b5e401a6a1be9fa8265803a0.png"
+      },
+      {
+        "HeroName": "Lich",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10038_ingame.7544eda066c78d85403c1a96ed096435f7343341.png"
+      },
+      {
+        "HeroName": "Ogre Magi",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10043_ingame.127396b9101aa4b8d7c32f6a763a30becf82d575.png"
+      },
+      {
+        "HeroName": "Omniknight",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10044_ingame.72d9db93559dfb850db41b5c56dfa9324d525473.png"
+      },
+      {
+        "HeroName": "Outworld Devourer",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10046_ingame.f93f51f512a39868f15a0adae08dbe098d2aa10b.png"
+      },
+      {
+        "HeroName": "Phantom Assassin",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10047_ingame.77fafd6442149fa870af2246ef2e44e41bba38c0.png"
+      },
+      {
+        "HeroName": "Pugna",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10048_ingame.727075c100ba4f0fa47e223c5fe671e920ed4d0c.png"
+      },
+      {
+        "HeroName": "Sniper",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10050_ingame.9f298145ea43e62aa3e55fc6ba180e8b32f7e029.png"
+      },
+      {
+        "HeroName": "Mazzie",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10052_ingame.983cb22019635500e40ab9aabe11fa0e4c9050c3.png"
+      },
+      {
+        "HeroName": "Prellex",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10053_ingame.8089df7555c623afc0e20e7873d62f0a8d5c3f66.png"
+      },
+      {
+        "HeroName": "Sven",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10054_ingame.28b35264859897d83c4ff6c4ceac138f194706c9.png"
+      },
+      {
+        "HeroName": "Treant Protector",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10056_ingame.22ad237a7bc3ac7c093590392823d8c6241ac30d.png"
+      },
+      {
+        "HeroName": "Sorla Khan",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10058_ingame.f40b33ccbbbb5749e3a418c9b9c20db908fe4472.png"
+      },
+      {
+        "HeroName": "Necrophos",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10059_ingame.3b8bbf6012bb0d2000f4f8fac91c8d2ac56a169a.png"
+      },
+      {
+        "HeroName": "Lion",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10060_ingame.3cc7cccf0863e3326fdb57fadc4be375ee9c5415.png"
+      },
+      {
+        "HeroName": "Skywrath Mage",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10062_ingame.9890e2113b5e86406485a425ecfc1d81f7b1be3c.png"
+      },
+      {
+        "HeroName": "Crystal Maiden",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10064_ingame.2ec56e566f1a8f6a6b800f1613f10515b4fd7d17.png"
+      },
+      {
+        "HeroName": "Zeus",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10065_ingame.66b980fa18c866fcfb5d704423f1a27e24822e3c.png"
+      },
+      {
+        "HeroName": "Magnus",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10067_ingame.9b4157f13dfd086f9cb1e2357985ccc384578119.png"
+      },
+      {
+        "HeroName": "Dark Seer",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10068_ingame.d551de559af77e7376f663a900d27f99b7616d6c.png"
+      },
+      {
+        "HeroName": "Legion Commander",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10069_ingame.659a7475aaa6e1cbeb8c7d86ab74d1e740a91a11.png"
+      },
+      {
+        "HeroName": "Ursa",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10070_ingame.42164bc5c798b36a6ddc65e2ad113e96486c4f3d.png"
+      },
+      {
+        "HeroName": "Storm Spirit",
+        "Url": "https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10536_ingame.0c88cb2ca6dcdab778a528e716d39210ed46c618.png"
+      }
+    ]
+  };
